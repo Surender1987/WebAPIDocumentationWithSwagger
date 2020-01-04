c74ffe650d7d76a24d8d098c4d1f0e4d21403b18
Surender1987 <38080919+Surender1987@users.noreply.github.com>
2020-01-04 03:19:18 -0800

    Add files via upload

--- WebAPIDocumentationWithSwagger/Controllers/WeatherForecastController.cs
+++ WebAPIDocumentationWithSwagger/Controllers/WeatherForecastController.cs
@@ -23,8 +23,26 @@ namespace WebAPIDocumentationWithSwagger.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Creates a TodoItem.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        /// POST /Todo
+        /// {
+        /// "id": 1,
+        /// "name": "Item1",
+        /// "isComplete": true
+        /// }
+        ///
+        /// </remarks>
+        /// <param name="item"></param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> Get(object item)
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
